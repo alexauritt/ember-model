fe8d87143d1f2debb91ff75f37102d1530d14b3e
Todd Evanoff <evanoff@gmail.com>
2013-06-09 11:27:17 -0400

    CP macro test for belongs_to.

--- packages/ember-model/tests/belongs_to_test.js
+++ packages/ember-model/tests/belongs_to_test.js
@@ -3,3 +3,10 @@ module("Ember.belongsTo");
 test("it exists", function() {
   ok(Ember.belongsTo);
 });
+
+test("is a CP macro", function() {
+  var Model = Ember.Model.extend();
+  var cp = Ember.belongsTo(Model, 'address');
+
+  ok(cp instanceof Ember.ComputedProperty);
+});
